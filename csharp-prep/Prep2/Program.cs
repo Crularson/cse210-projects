02ed8f687138c81f77122d475733abed19cbe619
Crularson <lar24046@byui.edu>
2026-01-08 21:45:43 -0700

    prep 1 and 2 completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string stringGrade = Console.ReadLine();
+        int intGrade = int.Parse(stringGrade);
+        string lettergrade = "";
+        if (intGrade >= 90)
+        {
+            lettergrade = "A";
+        }
+        else if (intGrade >= 80)
+        {
+            lettergrade = "B";
+        }
+        else if (intGrade >= 70)
+        {
+            lettergrade = "C";
+        }
+        else if (intGrade >= 60)
+        {
+            lettergrade = "D";
+        }
+        else
+        {
+            lettergrade = "F";
+        }
+        Console.WriteLine($"Your grade is {lettergrade}.");
+        if (intGrade >= 70)
+        {
+            Console.WriteLine("Good job, you passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You'll need to try harder on your homework next time...")
+        }
     }
 }
