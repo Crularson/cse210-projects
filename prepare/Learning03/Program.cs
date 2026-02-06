f737981e8bf3f18d6988cdf170b27e8e04296f43
Crularson <lar24046@byui.edu>
2026-02-05 22:48:08 -0700

    learning 03 complete.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,6 +5,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        Fraction fraction1 = new Fraction();
+        Random range = new Random();
+
+
+        for(int i=0; i<20; i++)
+        {
+            int topValue = range.Next(1,11);
+            int bottomValue = range.Next(1,11);
+            fraction1.SetTop(topValue);
+            fraction1.SetBottom(bottomValue);
+            Console.WriteLine($"Fraction 1: string: {fraction1.GetFractionString()} Number: {fraction1.GetDecimalValue()}");
+        }
     }
 }
