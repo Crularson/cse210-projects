1f173d9520d29b077dc67a552b21161493a2a34f
Crularson <lar24046@byui.edu>
2026-02-19 22:14:18 -0700

    prepare material complete. Learning 4 complete.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Cru Larson", "Math");
+
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment math = new MathAssignment("Cru Larson", "Multiplication", "4.3", "1-23");
+        Console.WriteLine(math.GetHomeworkList());
+
+        WritingAssignment english = new WritingAssignment("Cru Larson", "Historical Analysis", "World History and Me");
+        Console.WriteLine(english.GetWritingInformation());
     }
 }
